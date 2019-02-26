ba018fa58115622f2ba9c0127b8cd8fbf69f6c2e
Felipe Leal <felipewanderson2020@gmail.com>
2019-02-26 01:36:51 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/felipelleal/cursomc/services/ClienteService.java
@@ -0,0 +1,23 @@
+package com.felipelleal.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.felipelleal.cursomc.domain.Cliente;
+import com.felipelleal.cursomc.repositories.ClienteRepository;
+import com.felipelleal.cursomc.services.exception.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id:" + id + ", Tipo: "+ Cliente.class.getName()));
+	}
+}
---
+++ src/main/java/com/felipelleal/cursomc/services/EnderecoService.java
@@ -0,0 +1,23 @@
+package com.felipelleal.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.felipelleal.cursomc.domain.Endereco;
+import com.felipelleal.cursomc.repositories.EnderecoRepository;
+import com.felipelleal.cursomc.services.exception.ObjectNotFoundException;
+
+@Service
+public class EnderecoService {
+	
+	@Autowired
+	private EnderecoRepository repo;
+	
+	public Endereco buscar(Integer id) {
+		Optional<Endereco> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id:" + id + ", Tipo: "+ Endereco.class.getName()));
+	}
+}
